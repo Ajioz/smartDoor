7282aeff00c68e0c5781c3c5c4467c9874e04575
Sunday Ajiroghene <sunny.ajiroghene@gmail.com>
2024-04-29 10:29:15 +0100

    Updated email UX-UI to be Google, Yahoo, compatible, few bug left

--- client/web/backend/util/emailSender.js
+++ client/web/backend/util/emailSender.js
@@ -80,172 +80,72 @@ export const sendSingleEmail = async (
 };
 
 let getEmailTemplate = (token, host) => {
-  return `  <!DOCTYPE html>
-  <html lang="en">
-    <head>
-      <meta charset="UTF-8" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <title>Email Confirmation</title>
-      <style>
-        text:hover {
-          background-color: #096d8b;
-          cursor: pointer;
-          box-shadow: 0 0.4rem 1.4rem 0 rgba(86, 185, 235, 0.5);
-          transform: translateY(-0.1rem);
-          transition: transform 150ms;
+  return `<!DOCTYPE html>
+    <html lang="en">
+      <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Email Confirmation</title>
+        <style>
+        .btn:hover {
+            background-color: #096d8b;
+            box-shadow: 0 0.4rem 1.4rem 0 rgba(86, 185, 235, 0.5);
+            transform: translateY(-0.1rem);
+            transition: transform 150ms;
         }
       </style>
-    </head>
-    <body style="
-        font-family: sans-serif;
-        margin: 0;
-        padding: 0;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        min-height: 100vh;
-        background-color: #f1f8e9;" >
-      <div style="
-          display: flex;
-          flex-direction: column;
-          justify-content: space-between;
-          background-color: #f1f8e9;
-          border-radius: 5px;
-          padding: 30px 90px;
-          max-width: 600px;
-          width: 100%;
-          box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1); " >
-        <div style="
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            width: 100%;
-            margin: 10px;">
-          <img src="../img/logo4.png" alt="" style="width: 150px" />
-        </div>
-        <div style="
-            display: flex;
-            height: 3px;
-            background: rgb(20, 232, 101);
-            background: linear-gradient(
-              88deg,
-              rgba(20, 232, 101, 1) 0%,
-              rgba(26, 218, 187, 1) 8%,
-              rgba(11, 77, 228, 1) 17%,
-              rgba(177, 53, 233, 1) 31%,
-              rgba(219, 83, 236, 1) 44%,
-              rgba(191, 92, 237, 1) 44%,
-              rgba(241, 125, 207, 1) 55%,
-              rgba(226, 31, 61, 1) 67%,
-              rgba(233, 177, 87, 1) 75%,
-              rgba(235, 172, 73, 1) 87%,
-              rgb(196, 176, 4) 100%
-            ); "></div>
-        <div style="
-            display: flex;
-            flex-direction: column;
-            justify-content: flex-start;
-            align-items: center;
-            width: 100%;">
-          <h1 class="title" style="font-size: 18px; margin-left: 20px">
-            Confirm Your Email !
-          </h1>
-        </div>
-        <main style="line-height: 1.5">
-          <p style="
-              font-size: 12px;
-              font-family: Arial, Helvetica, sans-serif;
-              color: #56737c;">
-            Please confirm it's you.
-          </p>
-          <p style="
-              font-size: 12px;
-              font-family: Arial, Helvetica, sans-serif;
-              color: #56737c;" >
-            You will need to click the below button to proceed on platform
-            smartlock.io
-          </p>
-          <p style="
-              font-size: 12px;
-              font-family: Arial, Helvetica, sans-serif;
-              color: #56737c; ">
-            Please feel free to reach out to us anytime at care@smartlock.com for
-            any questions or inquiries. We would love to hear your feedback.
-          </p>
-          <p style="
-              font-size: 12px;
-              font-family: Arial, Helvetica, sans-serif;
-              color: #56737c;">
-            Thanks for helping us improve smartlock for you!
-          </p>
-        </main>
-        <div style="text-align: center; margin-top: 20px">
-          <a
-            href="http://${host}/api/user/${token}"
-            style="text-decoration: none; color: #007bff" >
-            <text
-              type="button"
-              style=" outline: none;
-                padding: 15px;
-                border-radius: 10px;
-                background-color: #56737c;
-                border: none;
-                color: #fff;
-                font-size: 12px;
-                transition: 0.5s;">Go to smartLock</text>
+      </head>
+      <body>
+        <div style="display: block;background-color: #f1f8e9; border-radius: 5px;  padding: 30px 90px; max-width: 600px;
+          width: 100%; margin: 0 auto; box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);">
+          <div style=" text-align: center; margin: 10px 0; ">
+            <img src="../img/logo4.png" alt="" />
+          </div>
+          <div style=" height: 3px;
+          background: rgb(20, 232, 101);
+          background: linear-gradient(88deg, rgba(20, 232, 101, 1) 0%, rgba(26, 218, 187, 1) 8%, rgba(11, 77, 228, 1) 17%, rgba(177, 53, 233, 1) 31%, rgba(219, 83, 236, 1) 44%, rgba(191, 92, 237, 1) 44%, rgba(241, 125, 207, 1) 55%, rgba(226, 31, 61, 1) 67%, rgba(233, 177, 87, 1) 75%, rgba(235, 172, 73, 1) 87%, rgb(196, 176, 4) 100%);"></div>
+          <div class="header">
+            <h1 style="text-align: center; color: #333; font-size: 14px;  font-size: 18px; margin: 20px;">
+                Confirm Your Email
+            </h1>
+          </div>
+          <main>
+            <p style="line-height: 1.5; font-size: 13px; color:#4b6a74; margin-bottom: 10px;">Please confirm it's you.</p>
+            <p style="line-height: 1.5; font-size: 13px; color:#4b6a74; margin-bottom: 10px;">
+              You will need to click the below button to proceed on platform
+              <a href="https://smartlock.io" style="color:#56737c; text-decoration: underline;">smartlock.io</a> 
+            </p>
+            <p style="line-height: 1.5; font-size: 13px; color:#4b6a74; margin-bottom: 10px;">
+              Please feel free to reach out to us anytime at <a href="mailto: care@smartlock.com" style="color:#56737c; text-decoration: underline;">care@smartlock.com</a> for
+              any questions or inquiries. We would love to hear your feedback.
+            </p>
+            <p style="line-height: 1.5; font-size: 13px; color:#4b6a74; margin-bottom: 10px;">Thanks for helping us improve smartlock for you!</p>
+          </main>
+          <div style="  text-align: center; margin-top: 20px;">
+          <a href= http://${host}/api/user/${token} style="text-decoration: none;">
+            <button class="btn" style="outline: none; padding: 15px; border-radius: 10px;background-color: #56737c;border:  none;color: #fff;font-size: 12px;margin: 40px auto;transition: 0.5s;cursor: pointer;display: inline-block;" >
+                Go to smartLock
+            </button>
           </a>
-          <h4 style="
-              color: #56737c;
-              font-weight: 800;
-              font-size: 12px !important;
-              margin-top: 50px;" >
-            smartLock, Inc.
-          </h4>
-          <p style="margin-bottom: 5px; font-size: 10px; color: #8c9597">
-            Vintage Road Suite 190
-          </p>
-          <p style="margin-bottom: 5px; font-size: 10px; color: #8c9597">
-            Los Gatos, CA 95232 United States
-          </p>
-          <p style="margin-bottom: 5px; font-size: 10px; color: #8c9597">
-            Ask about anything via care@smartlock.io, To unsubscribe, please click
-            here
-          </p>
-          <div>
-            <h4 style="
-                margin-bottom: 0;
-                color: #56737c;
-                font-weight: 800;
-                font-size: 12px !important;
-                margin-top: 20px;">
-              Follow us on
+            <h4 style="margin:0px auto 3px auto;color:#56737c;font-weight:800;font-size:12px; margin-top:0;">
+                smartLock, Inc.
             </h4>
-            <div style="display: flex; justify-content: center">
-              <a style="
-                  color: #8c9597;
-                  text-decoration: none;
-                  margin-right: 10px;
-                  font-size: 11px;"
-                href="#">LinkedIn</a>
-              <a style="
-                  color: #8c9597;
-                  text-decoration: none;
-                  margin-right: 10px;
-                  font-size: 11px;" 
-                  href="#">Twitter</a>
-              <a  style="
-                  color: #8c9597;
-                  text-decoration: none;
-                  margin-right: 10px;
-                  font-size: 11px;"
-                  href="#">Youtube</a>
+            <p style="margin: 0; margin-bottom: 5px; font-size: 10px; color: #8c9597;">Vintage Road Suite 190</p>
+            <p style="margin: 0; margin-bottom: 5px; font-size: 10px; color: #8c9597;">Los Gatos, CA 95232 United States</p>
+            <p style="margin: 0; margin-bottom: 5px; font-size: 10px; color: #8c9597;">
+              Ask about anything via care@smartlock.io, To unsubscribe, please <a href="https:smartlock.io" style="text-decoration: underline; color: #56737c;">click
+              here</a>
+            </p>
+            <div style="margin: 20px auto 0 auto;">
+              <h4 style="margin: 0;  color: #56737c; font-weight: 800; font-size: 12px;margin-top: 0;">Follow us on</h4>
+              <a style="color: #8c9597;text-decoration: none; margin-right: 10px; font-size: 11px;" href="#"> LinkedIn</a>
+              <a style="color: #8c9597;text-decoration: none; margin-right: 10px; font-size: 11px;" href="#"> Twitter</a>
+              <a style="color: #8c9597;text-decoration: none; margin-right: 10px; font-size: 11px;" href="#"> Youtube</a>
             </div>
           </div>
         </div>
-      </div>
-    </body>
-  </html>
-`;
+      </body>
+    </html>`;
 };
 
 const confirmEmail = (spam, token, host) => {
