85a7d81d6b976ef50e1de1d86fda564e7c5d0ac7
Sunday Ajiroghene <sunny.ajiroghene@gmail.com>
2024-05-12 17:00:38 +0100

    removed interface

--- client/web/interface/src/aws-iot-configuration.js
+++
@@ -1,7 +0,0 @@
-var awsIotConfiguration = {
-    endpoint: 'wss://a2wmf7df78xe7c-ats.iot.eu-west-2.amazonaws.com/mqtt',
-    region: 'eu-west-2',
-    poolId: 'eu-west-2:3e0fc3a6-8425-4f97-ab81-40cf4975552a',
-    host: 'a2wmf7df78xe7c-ats.iot.eu-west-2.amazonaws.com'
-  };
- module.exports = awsIotConfiguration;
